9dac4dfed0195e02aa607d238207f0fce49bbfb0
Chris Parker <chparker@atlassian.com>
2017-12-08 12:15:30 +0100

    added a return value

--- hiphop.py
+++ hiphop.py
@@ -9,3 +9,4 @@ def message_write():
     """
     requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification', headers=headers, params=params, data=data)"""
     requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S', headers=headers, data=data)
+    return 'ok'
