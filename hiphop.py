c40fea694417b0a41a3dd66da43cc3cd1090b364
Chris Parker <chparker@atlassian.com>
2017-12-08 12:09:20 +0100

    yet more oh more

--- hiphop.py
+++ hiphop.py
@@ -2,9 +2,10 @@ import requests
 import datetime
 
 #now = datetime.datetime.now()
+headers = {'Content-Type': 'application/json',}
+params = (('auth_token', 'hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S'),)
 
 def message_write():
-    headers = {'Content-Type': 'application/json',}
-    params = (('auth_token', 'hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S'),)
-    data = '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}'
-    requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification', headers=headers, params=params, data=data)
+    """data = '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}'
+    requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification', headers=headers, params=params, data=data)"""
+    requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S', headers=headers, data=data)
