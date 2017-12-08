c7164960f935f53380754c29b38e96a37897fee8
Chris Parker <chparker@atlassian.com>
2017-12-08 11:54:43 +0100

    more dman errors

--- hello.py
+++ hello.py
@@ -4,10 +4,11 @@ import hiphop
 app = Flask(__name__)
 
 @app.route("/")
-def action():
+def status():
     return "I am alive!"
 
 @app.route("/hit")
+def action():
     hiphop.message_write()
 
 if __name__ == "__main__":
