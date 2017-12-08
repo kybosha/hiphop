2183512b69114ebeea6a9dcde14bf088a5465597
Chris Parker <chparker@atlassian.com>
2017-12-08 11:14:57 +0100

    syntax fix

--- hiphop.py
+++ hiphop.py
@@ -5,4 +5,4 @@ now = datetime.datetime.now()
 url = "https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S"
 
 def message_write():
-    r = requests.post(url, headers={{"color":"green","message":now + "(yey)","notify":false,"message_format":"text"}' -H 'Content-Type: application/json'}) 
+    r = requests.post(url, headers={"color":"green","message":now + "(yey)","notify":false,"message_format":"text"}) 
