759bf2a47aec284c34c8e29c41c071563369c0ba
Chris Parker <chparker@atlassian.com>
2017-12-08 12:05:05 +0100

    improved request

--- hiphop.py
+++ hiphop.py
@@ -1,9 +1,10 @@
 import requests
 import datetime
 
-now = datetime.datetime.now()
-url = "https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S"
+#now = datetime.datetime.now()
 
 def message_write():
-    r = requests.post(url, headers={"color":"green","message":str(now) + " (yey)","notify":false,"message_format":"text"})
-    print(r.status_code)
+    headers = {'Content-Type': 'application/json',}
+    params = (('auth_token', 'hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S'),)
+    data = '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}'
+    requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification', headers=headers, params=params, data=data)
