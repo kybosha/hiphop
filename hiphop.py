7ef66cc6c18712e8a5ac1ee909ba41eb33bcd725
Chris Parker <chparker@atlassian.com>
2017-12-08 12:01:05 +0100

    datetime fix

--- hiphop.py
+++ hiphop.py
@@ -5,5 +5,5 @@ now = datetime.datetime.now()
 url = "https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S"
 
 def message_write():
-    r = requests.post(url, headers={"color":"green","message":now + "(yey)","notify":false,"message_format":"text"})
+    r = requests.post(url, headers={"color":"green","message":str(now) + " (yey)","notify":false,"message_format":"text"})
     print(r.status_code)
