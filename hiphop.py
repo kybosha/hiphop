5f0770d03d6fde60d3e8ddc89d3b72c56787b23f
Chris Parker <chparker@atlassian.com>
2017-12-08 12:10:56 +0100

    fingers crossed

--- hiphop.py
+++ hiphop.py
@@ -3,9 +3,9 @@ import datetime
 
 #now = datetime.datetime.now()
 headers = {'Content-Type': 'application/json',}
-params = (('auth_token', 'hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S'),)
+data = '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}'
 
 def message_write():
-    """data = '{"color":"green","message":"My first notification (yey)","notify":false,"message_format":"text"}'
+    """
     requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification', headers=headers, params=params, data=data)"""
     requests.post('https://atlassian.hipchat.com/v2/room/1622654/notification?auth_token=hYMOQTmiYuEKZ0JnwXlYCkFdys1iAuks0gzB6C4S', headers=headers, data=data)
